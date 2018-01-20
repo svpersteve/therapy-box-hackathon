be2950edd14c7850a14f4adf12454b8912b416a2
Steve Brewer <steve.brewer@futurelearn.com>
2018-01-20 15:23:00 +0000

    Get weather and store as variable in controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,12 @@
 class PagesController < ApplicationController
   def home
+    get_weather
+  end
+
+  private
+
+  def get_weather
+    get_weather = RestClient.get "http://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=#{ENV['secret']}"
+    @weather = ActiveSupport::JSON.decode(get_weather)
   end
 end
