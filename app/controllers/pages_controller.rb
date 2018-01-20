a7390abb7d9bde74affc01d9681628cf35172825
Steve Brewer <steve.brewer@futurelearn.com>
2018-01-20 16:18:08 +0000

    Add news headline to dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
   def home
     get_weather
+    get_news
   end
 
   private
@@ -8,5 +9,11 @@ class PagesController < ApplicationController
   def get_weather
     get_weather = RestClient.get "http://api.openweathermap.org/data/2.5/weather?lat=35&units=metric&lon=139&appid=#{ENV['secret']}"
     @weather = ActiveSupport::JSON.decode(get_weather)
+  end
+
+  def get_news
+    require 'rss'
+    require 'open-uri'
+    @rss = RSS::Parser.parse(open("http://feeds.bbci.co.uk/news/rss.xml").read, false).items.first
   end
 end
