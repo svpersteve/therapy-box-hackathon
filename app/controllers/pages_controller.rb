95702ab824f4abce3d2b52c232e40987b2433e93
Steve Brewer <steve.brewer@futurelearn.com>
2018-01-20 15:37:03 +0000

    Get temperate in celcius

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,7 +6,7 @@ class PagesController < ApplicationController
   private
 
   def get_weather
-    get_weather = RestClient.get "http://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=#{ENV['secret']}"
+    get_weather = RestClient.get "http://api.openweathermap.org/data/2.5/weather?lat=35&units=metric&lon=139&appid=#{ENV['secret']}"
     @weather = ActiveSupport::JSON.decode(get_weather)
   end
 end
